7fe9381d1cfe1c726df16282495fcb7eab0ec9c9
Torres1368 <roberth.torres1368@utc.edu.ec>
2025-05-12 11:29:07 -0500

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Urbanist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/ui/navbar";
+
+const urbanist = Urbanist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Ecommerce",
+  description: "Bienvenido a mi ecommerce donde puedes encontrar lo que buscas",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${urbanist.variable} ${geistMono.variable} antialiased`}>
+        <Navbar></Navbar>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/components/ui/navbar.tsx
@@ -0,0 +1,30 @@
+"use client"
+import { Heart, ShoppingCart, User } from "lucide-react";
+import { useRouter } from "next/navigation";
+import MenuList from "../menu-list";
+
+
+
+const Navbar = () => {
+    const router = useRouter()
+    return (
+        <div className="flex items-center justify-between p-4 mx-auto cursor-pointer sm:max-w-4xl md:max-w-6xl">
+            <h1 className="text-3xl" onClick={() => router.push('/')}>
+                E-commerce
+                <span className="font-bold text-blue-500">Dev</span>
+            </h1>
+            <div className="items-center justify-between hidden sm:flex">
+                <MenuList />
+            </div>
+            <div className="flex sm:hidden">
+                <p>items menu mobile</p>
+            </div>
+            <div className="flex items-center justify-between gap-2 sm:gap-7">
+                <ShoppingCart strokeWidth="1" className="cursor-pointer" onClick={() => router.push('/cart')}/>
+                <Heart strokeWidth="1" className="cursor-pointer" onClick={() => router.push('/loved-products')}/>
+                <User strokeWidth="1" className="cursor-pointer"/>
+            </div>
+        </div>
+    ); 
+}
+export default Navbar;
